7f2681082a3ad0572338aa902f2719bd0ead0b47
Knoks Knoksen <Knoksen@gmail.com>
2025-08-05 07:18:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className="font-body antialiased">
+      <body className="font-sans bg-gray-50 min-h-screen text-gray-900 antialiased">
         {children}
         <Toaster />
       </body>
